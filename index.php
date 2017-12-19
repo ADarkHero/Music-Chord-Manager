1e090129f6cbff0cacb02631ec0e5f17818652a4
Maik Riedlsperger <maik.riedlsperger@gmail.com>
2017-12-19 13:58:30 +0100

    It's not hard coded anymore
    So you can use this script for pretty much everything now, not only for a fixed use. Also it's much more flexible. Change the database structure and the frontend will change automatically with it.

--- index.php
+++ index.php
@@ -5,8 +5,13 @@
 	$db_user = "root";
 	$db_pass = "";
 	
-	global $pdo;
 	$pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+	
+	//Read all column names from database
+	$statement = $pdo->prepare("DESCRIBE $db_name");
+	$statement->execute();
+	$tables = $statement->fetchAll(PDO::FETCH_COLUMN);
+
 ?>
 
 <!DOCTYPE html>
@@ -18,109 +23,174 @@
 	</head>
 <body>
 
-<form action="index.php" method="post">
-	<input type="text" class="search" name="search" placeholder="Search..." autofocus>
-</form>
 
-<table class="table" style="width:100%">
 
-<tr>
-	<th class="id">ID</th>
-	<th class="artist">Artist</th>
-	<th class="title">Title</th>
-	<th class="text">Text</th>
-	<th class="notes">Notes</th>
-	<th class="link">Link</th>
-	<th class="editnew">Edit</th>
-	<th class="delete">Delete</th>
-</tr>
+
+
+
 
 <?php
-$statement = $pdo->prepare("DELETE FROM user WHERE UserID = :id");
-	$statement->bindParam(':id', $_GET["id"], PDO::PARAM_INT);   
-	$statement->execute();
+	displaySearch();
+
+	//Generate Table head
+	displayHead($tables);
+	
+	//Display a line to add a new entry	
+	if(!isset($_POST["search"]) || $_POST["search"] == ""){ displayNewEntry($pdo, $db_name, $tables); }	
 		
 	//If the delete button was used, delete something
-	if(isset($_GET["delete"]) && $_GET["delete"] != ""){ 
-		$statement = $pdo->prepare("DELETE FROM music WHERE MusicID = :id");
-		$statement->bindParam(':id', $_GET["delete"], PDO::PARAM_INT); 
-		$statement->execute();
-	}
+	if(isset($_GET["delete"]) && $_GET["delete"] != ""){ deleteEntry($pdo, $db_name, $tables); }
+	
 	//If the edit button was used, edit something
-	if(isset($_POST["edit"]) && $_POST["edit"] != ""){ 
-		$statement = $pdo->prepare("UPDATE music SET MusicID=:editId, MusicArtist=:editArtist, MusicTitle=:editTitle, MusicText=:editText, MusicNotes=:editNotes, MusicLink=:editLink WHERE MusicID=:editId");
-
-		$statement->bindParam(':editId', $_POST["editId"]);  
-		$statement->bindParam(':editArtist', $_POST["editArtist"]);     
-		$statement->bindParam(':editTitle', $_POST["editTitle"]);     
-		$statement->bindParam(':editText', $_POST["editText"]);     
-		$statement->bindParam(':editNotes', $_POST["editNotes"]);     
-		$statement->bindParam(':editLink', $_POST["editLink"]);   
-		$statement->execute();
-	}
+	if(isset($_POST["edit"]) && $_POST["edit"] != ""){ editEntry($pdo, $db_name, $tables); }
+	
 	//If the new button was used, add something
-	if(isset($_POST["new"]) && $_POST["new"] != ""){ 
-		$statement = $pdo->prepare("INSERT INTO music (MusicArtist, MusicTitle, MusicText, MusicNotes, MusicLink) VALUES (?, ?, ?, ?, ?)");
-		
-		$statement->execute([$_POST["newArtist"], $_POST["newTitle"], $_POST["newText"], $_POST["newNotes"], $_POST["newLink"]]);
-	}
+	if(isset($_POST["new"]) && $_POST["new"] != ""){ addEntry($pdo, $db_name, $tables); }
 	
 	//If the search was used, it should only show related entries
-	if(isset($_POST["search"]) && $_POST["search"] != ""){ 
-		$search = '%'.$_POST["search"].'%';	//Search term can be on the beginning, middle or end
-		$statement = $pdo->prepare("SELECT * FROM music WHERE MusicArtist LIKE :search OR MusicTitle LIKE :search OR MusicText LIKE :search OR MusicNotes LIKE :search ORDER BY MusicArtist");
-		$statement->bindParam(':search', $search);
-	}
-	else{ 
+	if(isset($_POST["search"]) && $_POST["search"] != ""){ $statement = searchEntries($pdo, $db_name, $tables); }
 	//No search -> Read everything
-		$statement = $pdo->prepare("SELECT * FROM music ORDER BY MusicArtist");
-	}
+	else{ $statement = readAllEntries($pdo, $db_name, $tables); }
 	
 	//Read all relevant entries from the database
-		$result = $statement->execute();
-		for($i = 1; $row = $statement->fetch(); $i++) {
+	displayEntries($pdo, $db_name, $tables, $statement);	
+	
+	
+		
+		
+
+		
+		
+		
+		
+		
+		
+		function deleteEntry($pdo, $db_name, $tables){
+			$sql = "DELETE FROM ".$db_name." WHERE ".$tables[0]." =".$_GET["delete"];
+			$statement = $pdo->prepare($sql);
+			$statement->execute();
+		}
+		
+		function addEntry($pdo, $db_name, $tables){
+			$sql = "INSERT INTO ".$db_name." (";
+			for($i = 0; $i < sizeof($tables); $i++){
+				$sql = $sql.$tables[$i].", ";
+			}
+			$sql = substr($sql, 0, -2);	//Cut last , 
+			$sql = $sql.") VALUES (";
+			for($i = 0; $i < sizeof($tables); $i++){
+				$sql = $sql.'"'.$_POST["new".$tables[$i]].'", ';
+			}
+			$sql = substr($sql, 0, -2);	//Cut last , 
+			$sql = $sql.")";
+					
+			$statement = $pdo->prepare($sql);
+			
+			$statement->execute();
+		}
+		
+		function editEntry($pdo, $db_name, $tables){
+			$sql = "UPDATE ".$db_name." SET ";
+			for($i = 0; $i < sizeof($tables); $i++){
+				$sql = $sql.$tables[$i].'="'.$_POST["edit".$tables[$i]].'", ';
+			}
+			$sql = substr($sql, 0, -2);	//Cut last , 
+			$sql = $sql." WHERE ".$tables[0].'="'.$_POST["edit".$tables[0]].'"';
+			
+			$statement = $pdo->prepare($sql);
+
+			$statement->execute();
+		}
+		
+		function searchEntries($pdo, $db_name, $tables){
+			$search = '%'.$_POST["search"].'%';	//Search term can be on the beginning, middle or end
+			$sql = "SELECT * FROM ".$db_name." WHERE ";
+			for($i = 0; $i < sizeof($tables); $i++){
+				$sql = $sql.$tables[$i].' LIKE "'.$search.'" OR ';
+			}
+			$sql = substr($sql, 0, -3); //Cut last "OR"
+			$statement = $pdo->prepare($sql);
+			
+			return $statement;
+		}
+		
+		function readAllEntries($pdo, $db_name, $tables){
+			//Base statement
+			$sql = "SELECT * FROM ".$db_name." ORDER BY ";
+			//Which order? If get sends an order, use it - else: sort by second table
+			if(isset($_GET["order"]) && $_GET["order"] != ""){ 
+				$sql = $sql.$_GET["order"];
+			}
+			else{
+				$sql = $sql.$tables[1];
+			}
+			
+			//Reverse sorting?
+			if(isset($_GET["desc"]) && $_GET["desc"] != ""){ 
+				$sql = $sql." DESC";
+			}
+						
+			$statement = $pdo->prepare($sql);
+			return $statement;
+		}
+		
+		
+		
+		
+		function displaySearch(){
+			echo'<form action="index.php" method="post">';
+				echo'<input type="text" class="search" name="search" placeholder="Search..." autofocus>';
+			echo'</form>';
+		}
+		
+		function displayHead($tables){
+			echo '<table class="table"><tr>';
+			for($i = 0; $i < sizeof($tables); $i++){
+				echo '<th class="table'.$i.'">'.$tables[$i].' <a href="index.php?order='.$tables[$i].'">▼</a> <a href="index.php?order='.$tables[$i].'&desc=true">▲</a></th>';
+			}
+			echo '<th class="tableeditnew">Edit</th>';
+			echo '<th class="tabledelete">Delete</th>';
+			echo "</tr>";
+		}
+		
+		function displayEntries($pdo, $db_name, $tables, $statement){
+			$result = $statement->execute();
+			for($i = 0; $row = $statement->fetch(); $i++) {
+				echo '<form action="index.php" method="post">';
+					echo "<tr>";
+						for($i = 0; $i < sizeof($tables); $i++){
+							echo '<td class="table'.$i.'"><textarea name="edit'.$tables[$i].'">'.$row[$i].'</textarea></td>';
+						}
+						echo '<td class="tableeditnew"><input type="submit" name="edit" value="EDIT"></button></td>'; 
+						echo '<td class="tabledelete"><a href="#" onclick="confirmDeletion('.$row[0].')">DELETE</a></td>'; 
+					echo "</tr>";
+				echo "</form>";
+			}
+		}
+		
+		function displayNewEntry($pdo, $db_name, $tables){
 			echo '<form action="index.php" method="post">';
 				echo "<tr>";
-					echo '<td class="id"><textarea name="editId">'.$row['MusicID'].'</textarea></td>';
-					echo '<td class="artist"><textarea name="editArtist">'.$row['MusicArtist'].'</textarea></td>';
-					echo '<td class="title"><textarea name="editTitle">'.$row['MusicTitle'].'</textarea></td>';
-					echo '<td class="text"><textarea name="editText">'.$row['MusicText'].'</textarea></td>';
-					echo '<td class="notes" ><textarea name="editNotes">'.$row['MusicNotes'].'</textarea></td>';
-					echo '<td class="link"><a href="'.$row['MusicLink'].'" target="_blank"><textarea name="editLink" class="linktext">'.$row['MusicLink'].'</textarea></a></td>';
-					echo '<td class="editnew"><input type="submit" name="edit" value="EDIT"></button></td>'; 
-					echo '<td class="delete"><a href="#" onclick="confirmDeletion('.$row['MusicID'].')">DELETE</a></td>'; 
+					for($i = 0; $i < sizeof($tables); $i++){
+						echo '<td class="table'.$i.'"><textarea name="new'.$tables[$i].'"></textarea></td>';
+					}
+					echo '<td class="tableeditnew"><input type="submit" name="new" value="NEW"></button></td>'; 
+					echo '<td class="tabledelete"></td>'; 
 				echo "</tr>";
 			echo "</form>";
 		}
-		
-		//Display a line to add a new entry
-		echo '<form action="index.php" method="post">';
-				echo "<tr>";
-					echo '<td class="id"><textarea name="newId"></textarea></td>';
-					echo '<td class="artist"><textarea name="newArtist"></textarea></td>';
-					echo '<td class="title"><textarea name="newTitle"></textarea></td>';
-					echo '<td class="text"><textarea name="newText"></textarea></td>';
-					echo '<td class="notes"><textarea name="newNotes" value=""></textarea></td>';
-					echo '<td class="link"><textarea name="newLink" value=""></textarea></td>';
-					echo '<td class="editnew"><input type="submit" name="new" value="NEW"></button></td>'; 
-					echo '<td class="delete"></td>'; 
-				echo "</tr>";
-			echo "</form>";
-		
-		echo '</table>';
-
 ?>
 
-<script>
-function confirmDeletion(id) {
-    var r = confirm("Do you really want to delete this entry?");
-	
-	if(r == true){
-		window.location = "index.php?delete="+id;
-		header('Location: index.php');
+	<script>
+	function confirmDeletion(id) {
+		var r = confirm("Do you really want to delete this entry?");
+		
+		if(r == true){
+			window.location = "index.php?delete="+id;
+			header('Location: index.php');
+		}
 	}
-}
 
-</script>
+	</script>
 
 </body>
